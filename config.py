c28a8c6dfffa89589886755f92d3653c6a135a51
Trojen1 <88619598+Trojen1@users.noreply.github.com>
2021-12-03 23:57:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "4091096"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "6bb0682b4af56456201c3b9d8b99c94a" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "2010848759:AAGoDbMXipmRpSK5KRSg6dsgka1KKD-eR-A" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
